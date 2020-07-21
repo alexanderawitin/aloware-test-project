fdab58e9dce22071facecfc71544e2f51a194486
Alexander Awitin Jr <alexanderawitin@gmail.com>
2020-07-22 00:04:36 +0800

    :seedling: Create Comment model and migration

---
+++ app/Comment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function commentable() {
+        return $this->morphTo();
+    }
+
+    public function replies() {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function parent() {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function user() {
+        $this->belongsTo(User::class);
+    }
+}
